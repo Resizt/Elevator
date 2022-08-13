282bd719bebc58424f576a9b55fbc5ad88019d06
Genesis Jackson <resiztdzn@gmail.com>
2022-08-13 14:57:42 -0400

    Update
    Nothing massive just fixing up this program and adding a workflow at the bottom

--- Elevator/Elevator.cpp
+++ Elevator/Elevator.cpp
@@ -3,46 +3,55 @@
 #include <iostream>
 #include <stdlib.h>
 #include <ksguid.h>
+#include <queue>
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 using namespace std;
 
-void called(bool&, int&);
-int elevator(bool&, int&);
+int called(bool&, int&); // User calling elevator0
+void elevator(bool&, int&);
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 int main() {
 	int floor;
 	bool pressed;
-
-	called(pressed, floor);
 	elevator(pressed, floor);
-	exit(0);
+
+	printf("Goodbye World");
+	exit(0);	
 }
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-void called(bool& pressed, int& floor) {
-	cout << "What floor are you on: ";
-	cin >> floor;
-	if (floor < 5 && floor > 0) {
-		pressed = true;
-	}
-}
-
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-int elevator(bool& pressed, int& floor) {
+int called(bool& pressed, int& floor) {
 	int elevatorcalled, realnum;
 	elevatorcalled = rand() % 1 + 10;
+}
 
-	for (int rng = 0; rng > 100; rng++) {
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
+void elevator(bool& pressed, int& floor) {
+	queue<int> FloorTravel;
+	bool eOff = true;
+
+	while (!eOff) {
+		FloorTravel.push(called(pressed, floor));
 	}
-
-	cout << elevatorcalled << endl;
 }
 
 // This is a test from visual studios
+
+
+/*
+Workflow
+Elevator sititng still
+Call function happens
+Set direction (if Currentfloor < ifFloorIsCalled) = Up 
+(if statement ifFloorisCalled while in motion) add to queue
+	(if Currentfloor < ifFloorIsCalled) = Up 
+When elevator reach floor (pop.front)
+if !queue.empty 
+
+*/
